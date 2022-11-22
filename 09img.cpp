01cc41665728bcf130e6a7ed5efd00a986f17ac6
Raul Mario <64203039+raulmrio28-git@users.noreply.github.com>
2022-11-22 15:40:02 +0200

    Use array pointers

--- 09img.cpp
+++ 09img.cpp
@@ -1,57 +1,61 @@
 /* //===========================================================\\
    || 09 Image Utilities Code - 09img.cpp                       ||
    ||-----------------------------------------------------------||
+   || 0.3 = 22 Nov. 2022 - Use array pointers instead of file as||
+   || input and vector as output.                               ||
    || 0.2 = 16 Aug. 2022 - Now use bit field for type (RLE, off-||
    || set)                                                      ||
    || 0.1 = 18 July 2022 - Implementing decode function         ||
    \\===========================================================// */
 
 #include <iostream>
-#include <fstream>
-#include <vector>
 #include "09img.h"
 
-int img09_decode (std::ifstream &file, std::vector<unsigned short>& out) {
+int img09_decode (unsigned char *in, unsigned short *out, int size) {
+	int outsize = 0;
 	unsigned char header, startoffset, info, bit_offset;
+	unsigned short *palettebank;
 	unsigned short pixel;
-	file.read((char*)&header, 1);
+	unsigned char *inend = in + size;
+	header = *in++;
 	if (header != 0x09 && header != 0x4f && header != 0x3b) {
 		printf("Invalid image\n");
 		return 2;
 	}
-	file.read((char*)&startoffset, 1);
-	file.seekg((startoffset * 2) + 2);
-
-	while(!file.eof()){
-		file.read((char*)&info, 1);
-		int old_pos;
+	startoffset = *in++;
+	palettebank = (unsigned short*)malloc(startoffset*2);
+	memcpy(palettebank, in, startoffset*2);
+	in += startoffset*2;
+	while(in < inend){
+		info = *in++;
 		if (info & IS_RLE) {	
 			if (info & USES_OFFSET_BYTE) {
-				file.read((char*)&bit_offset, 1);
-				int data_offset = 2 + (bit_offset*2);
-				old_pos = (int)file.tellg();
-				file.seekg(data_offset);
+				bit_offset = *in++;
+				pixel = palettebank[bit_offset];
 			}
-				file.read((char*)&pixel, 2);
-			if (info & USES_OFFSET_BYTE) {
-				file.seekg(old_pos);
+			else
+			{
+				pixel = *(unsigned short*)in;
+				in+=2;
 			}
-			for (int a=0; a < (info & COUNT_MAX); a++) {
-				out.push_back(pixel);
+			for (int a = 0; a < (info & COUNT_MAX); a++)
+			{
+				out[outsize] = pixel;
+				outsize++;
 			}
 		} else {
-			for (int a=0; a < (info & COUNT_MAX); a++) {
+			for (int a = 0; a < (info & COUNT_MAX); a++) {
 				if (info & USES_OFFSET_BYTE) {
-					file.read((char*)&bit_offset, 1);
-					int data_offset = 2 + (bit_offset*2);
-					old_pos = (int)file.tellg();
-					file.seekg(data_offset);
+					bit_offset = *in++;
+					pixel = palettebank[bit_offset];
 				}
-					file.read((char*)&pixel, 2);
-				if (info & USES_OFFSET_BYTE) {
-					file.seekg(old_pos);
-				} 
-				out.push_back(pixel);
+				else
+				{
+					pixel = *(unsigned short*)in;
+					in+=2;
+				}
+				out[outsize] = pixel;
+				outsize++;
 			}
 		}
 	}
--- 09img.h
+++ 09img.h
@@ -1,6 +1,8 @@
 /* //===========================================================\\
    || 09 Image Utilities Header - 09img.h                       ||
    ||-----------------------------------------------------------||
+   || 0.3 = 22 Nov. 2022 - Use array pointers instead of file as||
+   || input and vector as output.                               ||
    || 0.2 = 16 Aug. 2022 - Now use bit field for type (RLE, off-||
    || set)                                                      ||
    || 0.1 = 18 July 2022 - Implementing decode function         ||
@@ -10,4 +12,4 @@
 #define USES_OFFSET_BYTE 0x40 //01000000b
 #define COUNT_MAX 0x3f //00111111b
 
-int img09_decode (std::ifstream &file, std::vector<unsigned short>& out);
+int img09_decode (unsigned char *in, unsigned short *out, int size);
